78069e4b80968a8a2f799aaf5cf96473031ad66e
raiper34 <raipergm34@gmail.com>
2022-06-20 21:41:21 +0200

    test(select): unit tests for editor select component

--- projects/ngx-simple-text-editor/src/lib/components/editor-select/editor-select.component.spec.ts
+++ projects/ngx-simple-text-editor/src/lib/components/editor-select/editor-select.component.spec.ts
@@ -1,6 +1,10 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
 
-import { EditorSelectComponent } from './editor-select.component';
+import {EditorSelectComponent} from './editor-select.component';
+import {ExecCommand} from '../../models/exec-command';
+import {ToolbarItemType} from '../../models/button';
+import {By} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
 
 describe('EditorSelectComponent', () => {
   let component: EditorSelectComponent;
@@ -8,9 +12,10 @@ describe('EditorSelectComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ EditorSelectComponent ]
+      declarations: [EditorSelectComponent],
+      imports: [FormsModule]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -21,5 +26,31 @@ describe('EditorSelectComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit command', () => {
+    component.command.emit = jasmine.createSpy();
+    component.onCommand(ExecCommand.fontSize, 'val');
+
+    expect(component.command.emit).toHaveBeenCalledWith({command: ExecCommand.fontSize, value: 'val'});
+  });
+
+  it('should choose option and emit command', () => {
+    component.button = {
+      type: ToolbarItemType.Select, command: ExecCommand.fontSize, state: '1', items: [
+        {value: '1', label: 'Option 1'},
+        {value: '2', label: 'Option 2'},
+        {value: '3', label: 'Option 3'},
+      ]
+    };
+    component.command.emit = jasmine.createSpy();
+    fixture.detectChanges();
+
+    const selectElm = fixture.debugElement.query(By.css('select'));
+    selectElm.nativeElement.value = '2';
+    selectElm.nativeElement.dispatchEvent(new Event('change'));
+    fixture.detectChanges();
+
+    expect(component.command.emit).toHaveBeenCalledWith({command: ExecCommand.fontSize, value: '2'});
   });
 });
